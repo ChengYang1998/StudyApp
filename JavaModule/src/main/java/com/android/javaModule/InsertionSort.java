d0eb569fd0c89ae5e73fb2ad942e3548d5a33744
ChengYang <963255218@qq.com>
2022-09-28 16:17:15 +0800

    2-3 插入排序优化

--- JavaModule/src/main/java/com/android/javaModule/InsertionSort.java
+++ JavaModule/src/main/java/com/android/javaModule/InsertionSort.java
@@ -6,32 +6,20 @@ import java.util.Arrays;
 /**
  * author : cy
  * time   : 2022/9/26
- * desc   :
+ * desc   : 对大多数有序的数组,用插入排序法
  */
 public class InsertionSort {
     private InsertionSort() {
     }
 
+
+
     public static <E extends Comparable<E>> void sort(E[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            //将arr[i]插入到合适的位置
-            for (int j = i; j - 1 >= 0; j--) {
-                if (arr[j].compareTo(arr[j - 1]) < 0) {
-                    swap(arr, j, j - 1);
-                } else {
-                    break;
-                }
-            }
-
-        }
-
-    }
-
-    public static <E extends Comparable<E>> void sort2(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
             //将arr[i]插入到合适的位置 暂存 寻找插入位置 后面的元素后移
             E t = arr[i];
             int j;
+            //存在内层循环终止的情况
             for (j = i; j - 1 >= 0 && t.compareTo(arr[j - 1]) < 0; j--) {
                 //默认将元素后移
                 arr[j] = arr[j - 1];
@@ -42,21 +30,25 @@ public class InsertionSort {
     }
 
 
-    private static <E> void swap(E[] arr, int i, int j) {
-        E t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-
     public static void main(String[] args) {
         int[] dataSize = {10000, 100000};
         for (int n : dataSize) {
+            System.out.println("random array: ");
             Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
             Integer[] arr2 = Arrays.copyOf(arr,arr.length);
 
 
             SortingHelper.sortTest("InsertionSort", arr);
-            SortingHelper.sortTest("InsertionSort2", arr2);
+            SortingHelper.sortTest("SelectionSort", arr2);
+
+            System.out.println("Order array: ");
+
+            arr = ArrayGenerator.generateOrderedArray(n);
+            arr2 = Arrays.copyOf(arr,arr.length);
+
+            SortingHelper.sortTest("InsertionSort", arr);
+            SortingHelper.sortTest("SelectionSort", arr2);
+
 
         }
 
--- JavaModule/src/main/java/com/android/javaModule/SortingHelper.java
+++ JavaModule/src/main/java/com/android/javaModule/SortingHelper.java
@@ -9,8 +9,9 @@ public class SortingHelper {
     private SortingHelper() {
     }
 
-    public static <E extends Comparable> boolean isSorted(E[] arr) {
+    public static <E extends Comparable<E>> boolean isSorted(E[] arr) {
         for (int i = 1; i < arr.length; i++) {
+
             if (arr[i - 1].compareTo(arr[i]) > 0) {
                 return false;
             }
@@ -21,19 +22,22 @@ public class SortingHelper {
     }
 
 
-    public static <E extends Comparable> void sortTest(String sortName, E[] arr) {
+    public static <E extends Comparable<E>> void sortTest(String sortName, E[] arr) {
         long startTime = System.nanoTime();
         if (sortName.equals("SelectionSort")) {
             SelectionSort.sort(arr);
         } else if (sortName.equals("InsertionSort")) {
             InsertionSort.sort(arr);
-        }else if (sortName.equals("InsertionSort2")) {
-            InsertionSort.sort2(arr);
+        } else if (sortName.equals("InsertionSortOP")) {
+            InsertionSortOP.sort(arr);
         }
         long endTime = System.nanoTime();
 
         double time = (endTime - startTime) / 1000000000.0;
         if (!SortingHelper.isSorted(arr)) {
+            for (E in : arr) {
+            System.out.println(in);
+        }
             throw new RuntimeException(sortName + " failed");
         }
         System.out.println(String.format("%s,n=%d:%f", sortName, arr.length, time));
