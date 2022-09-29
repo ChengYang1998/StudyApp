74f14679f08b44468e220e285424be80c72a9da8
ChengYang <963255218@qq.com>
2022-09-29 15:21:58 +0800

    数组类增删改查

---
+++ JavaModule/src/main/java/com/android/array/Array.java
@@ -0,0 +1,176 @@
+package com.android.array;
+
+/**
+ * author : cy
+ * time   : 2022/9/28
+ * desc   :
+ */
+public class Array {
+    private int[] data;
+    private int size;
+
+    /**
+     * 构造函数
+     *
+     * @param capacity
+     */
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    /**
+     * 无参构造,默认容量为10
+     */
+    public Array() {
+        this(10);
+    }
+
+    /**
+     * @return 数组元素个数
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @return 数组容量
+     */
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    /**
+     * 在index个位置插入新元素e
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("Array is full");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index < 0 || index > size");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+
+        }
+        data[index] = e;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array:size = %d , capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+    public int[] getData() {
+
+        return data;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index < 0 || index > size");
+        }
+
+        return data[index];
+    }
+
+    public void set(int index, int e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index < 0 || index > size");
+        }
+
+        data[index] = e;
+    }
+
+    /**
+     * @param e
+     * @return 数组是否包含元素e
+     */
+    public boolean contains(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param e
+     * @return 数组中元素e的索引, 不存在则返回-1
+     */
+    public int find(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 从数组中删除index位置的元素
+     *
+     * @param index
+     * @return 删除的元素
+     */
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index < 0 || index > size");
+        }
+        int ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        return ret;
+    }
+
+    public int removeFirst() {
+        return remove(0);
+    }
+
+    public int removeLast() {
+        return remove(size - 1);
+    }
+
+    /**
+     * 删除数组中第一个元素e
+     *
+     * @param e
+     */
+    public void removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+}
