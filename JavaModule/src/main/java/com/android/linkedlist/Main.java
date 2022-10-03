44c635c1de4c9fd0e20dec94ae3bb3c8307c84b2
ChengYang <963255218@qq.com>
2022-10-03 20:38:53 +0800

    设计实现链表

---
+++ JavaModule/src/main/java/com/android/linkedlist/Main.java
@@ -0,0 +1,30 @@
+package com.android.linkedlist;
+
+
+/**
+ * author : cy
+ * time   : 2022/10/3
+ * desc   :
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}
