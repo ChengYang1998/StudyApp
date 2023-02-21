b7630970d7af0a2f1f37aa92bb38d1cab2ef59b5
Chip <89969815+ChengYang1998@users.noreply.github.com>
2023-02-21 14:32:16 +0800

    实现插入排序

--- JavaModule/src/main/java/com/android/sort/InsertionSort.java
+++ JavaModule/src/main/java/com/android/sort/InsertionSort.java
@@ -29,13 +29,28 @@ public class InsertionSort {
         }
     }
 
+    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+        for (int i = l; i <= r; i++) {
+            //将arr[i]插入到合适的位置 暂存 寻找插入位置 后面的元素后移
+            E t = arr[i];
+            int j;
+            //存在内层循环终止的情况
+            for (j = i; j - 1 >= l && t.compareTo(arr[j - 1]) < 0; j--) {
+                //默认将元素后移
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = t;
+
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] dataSize = {10000, 100000};
         for (int n : dataSize) {
             System.out.println("random array: ");
             Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-            Integer[] arr2 = Arrays.copyOf(arr,arr.length);
+            Integer[] arr2 = Arrays.copyOf(arr, arr.length);
 
 
             SortingHelper.sortTest("InsertionSort", arr);
@@ -44,7 +59,7 @@ public class InsertionSort {
             System.out.println("Order array: ");
 
             arr = ArrayGenerator.generateOrderedArray(n);
-            arr2 = Arrays.copyOf(arr,arr.length);
+            arr2 = Arrays.copyOf(arr, arr.length);
 
             SortingHelper.sortTest("InsertionSort", arr);
             SortingHelper.sortTest("SelectionSort", arr2);
