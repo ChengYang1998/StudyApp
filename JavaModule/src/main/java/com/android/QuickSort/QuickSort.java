3c3bd9ea036a0d97d3cf18a5e8628c558cf4a86c
Chip <89969815+ChengYang1998@users.noreply.github.com>
2023-02-21 17:05:39 +0800

    快速排序

---
+++ JavaModule/src/main/java/com/android/QuickSort/QuickSort.java
@@ -0,0 +1,22 @@
+package com.android.QuickSort;
+
+/**
+ * author : Chip
+ * time   : 2023/2/21
+ * desc   : 快速排序
+ */
+public class QuickSort {
+    private QuickSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] arr, int l, int r) {
+
+        // arr[l+1...j] < v ; arr[j+1...i] > v
+        return 0;
+
+    }
+}
